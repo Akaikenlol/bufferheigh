ce71acca13fd8aea7e20d31b19e1d7b45e51f256
Akaikenlol <67327997+Akaikenlol@users.noreply.github.com>
2020-07-05 02:05:55 +0630

    Add files via upload

---
+++ BufferHeight/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BufferHeight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The current buffer height is {0} rows .",Console.BufferHeight);
+            Console.WriteLine("The current buffer width id {0} column .",Console.BufferWidth);
+        }
+    }
+}
